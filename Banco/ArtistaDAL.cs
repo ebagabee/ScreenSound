da3ccd9cef1c8a9a566aa3cff79cdb2824a514be
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-06-25 13:59:42 -0300

    Adicionando Entity Framework ao projeto

---
+++ Banco/ArtistaDAL.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Data.SqlClient;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco
+{
+    internal class ArtistaDAL
+    {
+        public IEnumerable<Artista> Listar()
+        {
+            using var context = new ScreenSoundContext();
+            return context.Artistas.ToList();
+        }
+
+        public void Adicionar(Artista artista)
+        {
+            using var connection = new Connection().ObterConexao();
+            connection.Open();
+
+            string sql = "INSERT INTO Artistas (nome, FotoPerfil, Bio) VALUES (@nome, @perfilPadrao, @bio)";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@nome", artista.Nome);
+            command.Parameters.AddWithValue("@perfilPadrao", artista.FotoPerfil);
+            command.Parameters.AddWithValue("@bio", artista.Bio);
+        
+            int retorno = command.ExecuteNonQuery();
+            Console.WriteLine($"Linhas afetadas: {retorno}");
+        }
+
+        public void Atualizar(Artista artista) 
+        {
+            using var connection = new Connection().ObterConexao();
+            connection.Open();
+
+            string sql = $"UPDATE Artistas SET Nome = @nome, Bio = @bio WHERE Id = @id";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@nome", artista.Nome);
+            command.Parameters.AddWithValue("@bio", artista.Bio);
+            command.Parameters.AddWithValue("@id", artista.Id);
+
+            int retorno = command.ExecuteNonQuery();
+            Console.WriteLine($"Linhas afetadas: {retorno}");
+        }
+
+        public void Deletar(Artista artista)
+        {
+            using var connection = new Connection().ObterConexao();
+            connection.Open();
+
+            string sql = $"DELETE FROM Artistas WHERE Id = @id";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+            
+            command.Parameters.AddWithValue("@id", artista.Id);
+
+            int retorno = command.ExecuteNonQuery();
+            Console.WriteLine($"Linhas afetadas: {retorno}");
+        }
+    }
+}
---
+++ Banco/ScreenSoundContext.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco
+{
+    public class ScreenSoundContext : DbContext
+    {
+        public DbSet<Artista> Artistas { get; set; }
+        private string connectionString = "Server=localhost,1433;Database=ScreenSound;User Id=sa;Password=78545632@Abc;Encrypt=False;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
